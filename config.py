b3c6a6a1a43bc191efb560101a603caf8e7f4547
Joseph Adediji <josylado@gmail.com>
2019-09-13 09:27:37 +0300

    Add login, registration and user profile functionalities

--- config.py
+++ config.py
@@ -1,5 +1,6 @@
 import os
 
+
 class Config:
     '''
     General configuration parent class
@@ -7,6 +8,14 @@ class Config:
     MOVIE_API_BASE_URL ='https://api.themoviedb.org/3/movie/{}?api_key={}'
     MOVIE_API_KEY = os.environ.get('MOVIE_API_KEY')
     SECRET_KEY = os.environ.get('SECRET_KEY')
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://josylad:p@localhost/watchlist'
+    UPLOADED_PHOTOS_DEST = 'app/static/photos'
+#  email configurations
+    MAIL_SERVER = 'smtp.googlemail.com'
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+    MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
+    MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
 
 
 class ProdConfig(Config):
