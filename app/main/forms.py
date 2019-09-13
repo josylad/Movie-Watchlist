17620cdde9a4f1816b750ac4c651a4ee79a96318
Joseph Adediji <josylado@gmail.com>
2019-09-13 09:26:44 +0300

    Add registration and login forms function

--- app/main/forms.py
+++ app/main/forms.py
@@ -1,10 +1,15 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, TextAreaField, SubmitField
-# from wtforms.validators import Required
+from wtforms.validators import Required
 
 
 class ReviewForm(FlaskForm):
 
     title = StringField('Review Title')
     review = TextAreaField('Movie Review')
+    submit = SubmitField('Submit')
+    
+    
+class UpdateProfile(FlaskForm):
+    bio = TextAreaField('Tell us about you.', validators = [Required()])
     submit = SubmitField('Submit')
